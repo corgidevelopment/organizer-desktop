238edad49531279cfd8d4b6c1ddc5bffb24e668b
Anton Rhein <anton.rhein@sbk.org>
2025-12-16 12:40:59 +0100

    Weektable WIP Fix Grid Lines

--- src/component/week-table-item/WeekTableItem.tsx
+++ src/component/week-table-item/WeekTableItem.tsx
@@ -1,4 +1,4 @@
-import './week-table-item.css'
+import './week-table-item.css';
 import { CalendarIcon } from '../calendar-icon/CalendarIcon.tsx';
 
 export type TypeA = {
@@ -12,13 +12,15 @@ export type WeekViewItemProps = {
   dayOfDate: number
 }
 
-export function WeekTableItem({typeAs, dayOfDate, dayName}: WeekViewItemProps) {
+export function WeekTableItem({ typeAs, dayOfDate, dayName }: WeekViewItemProps) {
   return (<>
     <div className="bg-white col-start-1 col-end-2 week-table week-table-item week-table-item-icon p-1">
       <CalendarIcon border={false} dayName={dayName} dayNo={dayOfDate} />
     </div>
-    {typeAs.map(({id, text}) => <div
-      className="
+    <div className="separator vertical"></div>
+    {typeAs.map(({ id, text }) => <>
+      <div
+        className="
         col-span-1
         text-left
         min-h-12
@@ -30,9 +32,13 @@ export function WeekTableItem({typeAs, dayOfDate, dayName}: WeekViewItemProps) {
         justify-center
         week-table
         week-table-item
-        week-table-item-element"
-      key={id}>
-      <span className="block">{text}</span>
-    </div>)}
-  </>)
+        week-table-item-element
+        px-2
+        "
+        key={id}>
+        <span className="block">{text}</span>
+      </div>
+      <div className="separator vertical"></div>
+    </>)}
+  </>);
 }
--- src/component/week-table/WeekTable.tsx
+++ src/component/week-table/WeekTable.tsx
@@ -5,22 +5,21 @@ export function WeekTable() {
   return (<div
     className="
       grid
-      auto-rows-fr
       items-center
       w-fit
       justify-items-start
       min-h-[300px]
       week-table week-table-grid
       min-w-[300px]
-      border-gray-300
-      border
       m-2
       "
   >
+    <div className="separator col-span-full"></div>
     <WeekTableItem
       typeAs={[{ id: '1', text: 'Schießstand' }, { id: '2', text: 'Homeoffice' }]}
       dayName="Montag"
       dayOfDate={1} />
+    <div className="separator col-span-full"></div>
     <WeekTableItem
       typeAs={[{ id: '3', text: 'Sport' }, { id: '4', text: 'Office' }, {
         id: '14',
@@ -28,25 +27,31 @@ export function WeekTable() {
       }]}
       dayName="Dienstag"
       dayOfDate={2} />
+    <div className="separator col-span-full"></div>
     <WeekTableItem
       typeAs={[{ id: '5', text: 'Fotos K3' }, { id: '6', text: 'Homeoffice' }]}
       dayName="Mittwoch"
       dayOfDate={3} />
+    <div className="separator col-span-full"></div>
     <WeekTableItem
       typeAs={[{ id: '7', text: 'Weihnachtsfeier' }, { id: '8', text: 'Office' }]}
       dayName="Donnerstag"
       dayOfDate={4} />
+    <div className="separator col-span-full"></div>
     <WeekTableItem
       typeAs={[{ id: '9', text: 'Hemden bügeln' }, { id: '10', text: 'Office' }]}
       dayName="Freitag"
       dayOfDate={5} />
+    <div className="separator col-span-full"></div>
     <WeekTableItem
       typeAs={[{ id: '11', text: 'Kreisversammlung' }]}
       dayName="Samstag"
       dayOfDate={6} />
+    <div className="separator col-span-full"></div>
     <WeekTableItem
       typeAs={[{ id: '13', text: 'Sport' }]}
       dayName="Sonntag"
       dayOfDate={7} />
+    <div className="separator col-span-full"></div>
   </div>);
 }
